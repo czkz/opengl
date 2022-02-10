ec74ef809a44a9bf80d0d539d49184cac48d8040
Dmitry <deekovalev@gmail.com>
2022-02-10 22:31:42 +0300

    Blinn-phong

--- lighting.glsl
+++ lighting.glsl
@@ -11,6 +11,19 @@ vec3 phong(vec3 diffuseColor, vec3 specularColor, vec3 toLight, vec3 normal, vec
     return diffuseStrength * diffuseColor + specularStrength * specularColor;
 }
 
+vec3 blinn(vec3 diffuseColor, vec3 specularColor, vec3 toLight, vec3 normal, vec3 toEye, float gloss) {
+    vec3 L = normalize(toLight);
+    vec3 N = normalize(normal);
+    vec3 V = normalize(toEye);
+    vec3 H = normalize((L + V) / 2);
+
+    float diffuseStrength = max(0.0, dot(L, N));
+    float specularStrength = max(0.0, dot(N, H));
+    specularStrength = pow(specularStrength, exp2(gloss * 16));
+
+    return diffuseStrength * diffuseColor + specularStrength * specularColor;
+}
+
 float fresnel(vec3 normal, vec3 toEye) {
     vec3 N = normalize(normal);
     vec3 V = normalize(toEye);
